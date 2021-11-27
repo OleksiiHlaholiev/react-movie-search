2dc4e983d3473363019a7c4a113c363a76d79a21
oleksiiHlaholiev <oleksii.hlaholiev@magnise.com.ua>
2021-11-28 00:38:25 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1 className={'app-header'}>react-movie-search is coming soon ... !</h1>
+    </div>
+  );
+}
+
+export default App;
