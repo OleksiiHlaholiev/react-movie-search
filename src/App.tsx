99317c846fed1e94dd297c9e768cee683514a32e
oleksiiHlaholiev <oleksii.hlaholiev@magnise.com.ua>
2021-11-28 01:05:57 +0200

    test commit: updated app-header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.scss';
 const App = () => {
   return (
     <div className="app">
-      <h1 className={'app-header'}>react-movie-search is coming soon ... !</h1>
+      <h1 className={'app-header'}>React Movie Search is coming soon ... !</h1>
     </div>
   );
 }
