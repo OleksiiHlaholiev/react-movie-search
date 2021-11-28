b9763a88489bd0d38551c55d411b10e2be74d964
oleksiiHlaholiev <oleksii.hlaholiev@magnise.com.ua>
2021-11-28 08:38:36 +0200

    added MovieSearchPage - start of logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import MovieSearchPage from "./components/MovieSearchPage/MovieSearchPage";
 
 const App = () => {
   return (
     <div className="app">
-      <h1 className={'app-header'}>React Movie Search is coming soon ... !</h1>
+      <MovieSearchPage />
     </div>
   );
 }
---
+++ src/components/MovieSearchPage/MovieSearchPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './MovieSearchPage.scss';
+
+export default function MovieSearchPage() {
+    return (
+      <div className={'movie-search-page'}>
+          <h1 className={'app-header'}>React Movie Search is coming soon ... !</h1>
+      </div>
+    );
+};
