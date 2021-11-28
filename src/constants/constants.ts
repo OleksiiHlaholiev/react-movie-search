ca9db8c21752f015a309458cbe286ac5eb5aaf18
oleksiiHlaholiev <oleksii.hlaholiev@magnise.com.ua>
2021-11-28 10:05:11 +0200

    added constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,11 @@
 export const URL_API_BASE = 'https://api.themoviedb.org/3';
 export const API_KEY = '62b719d81284900a2580408f52cc3d78';
+
+export const PAGINATION_ITEMS_PER_PAGE = 20;
+export const MIN_SEARCH_QUERY_LENGTH = 1;
+export const TIME_PROTECTION_MS = 1000;
+export const MOBILE_WIDTH = 500;
+export const DESCRIPTION_SYMBOLS_QUANTITY = 200;
+export const IMG_W185_H278_PATH_BASE = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/';
+export const IMG_W350_H196_PATH_BASE = 'https://image.tmdb.org/t/p/w350_and_h196_bestv2/';
+export const IMG_W300_H450_PATH_BASE = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/';
