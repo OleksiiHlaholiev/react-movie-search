2c2943c5b502f0b81ad476f5f60d842db4382c6c
oleksiiHlaholiev <oleksii.hlaholiev@magnise.com.ua>
2021-11-28 11:39:55 +0200

    MovieSearchPage: added renderSearchResults/renderSearchResultItem

--- src/components/MovieSearchPage/MovieSearchPage.tsx
+++ src/components/MovieSearchPage/MovieSearchPage.tsx
@@ -1,6 +1,13 @@
 import React, {useEffect, useState} from "react";
-import { getMovies } from "../../services/api";
-import {TIME_PROTECTION_MS} from "../../constants/constants";
+import {getMovies} from "../../services/api";
+import {
+    DESCRIPTION_SYMBOLS_QUANTITY,
+    IMG_W185_H278_PATH_BASE,
+    IMG_W350_H196_PATH_BASE,
+    MOBILE_WIDTH,
+    TIME_PROTECTION_MS
+} from "../../constants/constants";
+import {monthDecoder} from "../../helpers/helpers";
 import SearchIcon from "../../assets/images/icon-search.png";
 import '../../styles/main.scss';
 import './MovieSearchPage.scss';
@@ -57,13 +64,14 @@ export default function MovieSearchPage() {
                            id="search-input"
                            className="form-control"
                            placeholder="Введите название фильма"
+                           value={tmpCustomerSearch}
                            autoComplete="off"
                            onChange={(e) => setTmpCustomerSearch(e?.target?.value)}
                     />
                     <div className="input-group-append">
                         <button type="submit" id="search-btn" className="btn btn-outline-secondary">
                             <i className="icon icon-search">
-                                <img src={SearchIcon} alt="icon-search" />
+                                <img src={SearchIcon} alt="icon-search"/>
                             </i>
                         </button>
                     </div>
@@ -71,6 +79,79 @@ export default function MovieSearchPage() {
             </form>
         );
     };
+
+    const renderSearchResultItem = (itemData: any, itemIndex: number) => {
+        const {id, title, release_date, vote_average, overview, poster_path} = itemData;
+
+        let tempDateStr;
+
+        try {
+            const tempDate = new Date(release_date);
+
+            tempDateStr = tempDate.getDate() + ' ' + monthDecoder(tempDate.getMonth()) + ' ' + tempDate.getFullYear()
+        } catch (error) {
+            tempDateStr = release_date;
+        }
+
+        const tempDescrStr = overview.length > DESCRIPTION_SYMBOLS_QUANTITY ?
+            overview.slice(0, DESCRIPTION_SYMBOLS_QUANTITY - 3) + '...' : overview;
+
+        const currentImgBase = window.innerWidth > MOBILE_WIDTH ? IMG_W185_H278_PATH_BASE : IMG_W350_H196_PATH_BASE;
+
+        return (
+            <div className="item" key={`item-${id}`}>
+                <div className="img-cont">
+                    <a className="link" href="" target="_blank">
+                        <img className="poster"
+                             src={`${currentImgBase}${poster_path}`}
+                             alt="video img"
+                        />
+                    </a>
+                </div>
+                <div className="info-cont">
+                    <a className="link" href="" target="_blank">
+                        <h3 className="name">{title}</h3>
+                    </a>
+                    <p className="date">{tempDateStr}</p>
+                    <p className="rating">{`Рейтинг: ${vote_average}`}</p>
+                    <p className="description">{tempDescrStr}</p>
+                    <p className="detail-info">
+                        <a className="link" href="" target="_blank">Подробнее</a>
+                    </p>
+                </div>
+            </div>
+        );
+    };
+
+    const renderSearchResults = (data: any) => {
+        const isDataNotEmpty = data && data?.length;
+        const title = isDataNotEmpty ? 'Найденные фильмы' : 'По вашему запросу ничего не найдено';
+
+        return (
+            <div className="search-results" id="search-results">
+                <h2 className="results-title">{title}</h2>
+
+                {isDataNotEmpty && (
+                    <div className="results-cont">
+                        {data?.map((itemData: any, itemIndex: number) => renderSearchResultItem(itemData, itemIndex))}
+                    </div>
+                )}
+
+                {/* TODO: the pagination */}
+                {/*<ul className="pagination justify-content-center">
+                  <li className="page-item spec disabled prev-page" data-page="prev">
+                      <span className="page-link">Previous</span>
+                  </li>
+                  <li className="page-item active template" data-page="1">
+                      <span className="page-link">1</span></li>
+                  <li className="page-item spec disabled next-page">
+                      <span className="page-link" data-page="next">Next</span>
+                  </li>
+              </ul>*/}
+            </div>
+        );
+    };
+
 
     return (
         <section className="movie-search-section">
@@ -80,6 +161,8 @@ export default function MovieSearchPage() {
                 </h1>
 
                 {renderCustomerSearchFilter()}
+
+                {renderSearchResults(data?.results)}
             </div>
         </section>
     );
